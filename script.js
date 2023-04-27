5301f6f6df710ccf582c01600c8418c7edd6bba6
jagrutiburde0413 <124602129+jagrutiburde0413@users.noreply.github.com>
2023-04-27 23:01:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
-//your code here
+// Get the elements for the clock hands
 const hourHand = document.querySelector('.hour-hand');
 const minuteHand = document.querySelector('.min-hand');
 const secondHand = document.querySelector('.second-hand');
 
-function setDate() {
+function setClock() {
+  // Get the current time
   const now = new Date();
 
+  // Calculate the position of the hour hand
+  const hours = now.getHours() % 12; // Convert to 12-hour format
+  const hoursDegrees = ((hours / 12) * 360) + 90; // Convert to degrees
+
+  // Calculate the position of the minute hand
+  const minutes = now.getMinutes();
+  const minutesDegrees = ((minutes / 60) * 360) + 90;
+
+  // Calculate the position of the second hand
   const seconds = now.getSeconds();
   const secondsDegrees = ((seconds / 60) * 360) + 90;
-  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
-  const minutes = now.getMinutes();
-  const minutesDegrees = ((minutes / 60) * 360) + ((seconds / 60) * 6) + 90;
-  minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
-
-  const hours = now.getHours();
-  const hoursDegrees = ((hours / 12) * 360) + ((minutes / 60) * 30) + 90;
+  // Set the rotation of the clock hands
   hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+  minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 }
 
-setInterval(setDate, 1000);
+// Call the setClock function every second to update the clock hands
+setInterval(setClock, 1000);
